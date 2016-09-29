a176561e2009f0d602be6090a5d362080f43672d
rohitnailwal <rnailwal@gmail.com>
2016-09-29 17:11:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req, res){
-    res.sendFile(path.join(__dirname, 'ui','article-one.html'));
+app.get('/article-one.html',function(req, res){
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
-app.get('/article-two',function(req, res){
-   res.sendFile(path.join(__dirname, 'ui','article-two.html'));
+app.get('/article-two.html',function(req, res){
+   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three',function(req, res){
