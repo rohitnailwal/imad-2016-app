8df6e0843a81ac4805296515b8da9def4bb86a92
rohitnailwal <rnailwal@gmail.com>
2016-10-17 23:49:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var articles={
                                 This is the content of my first article. This is the content of my first article. This is the content of my first article.
                                 This is the content of my first article. This is the content of my first article. This is the content of my first article.
                             </p>`
-                                };
+                                },
                 'article-two':{
                     title:'article two',
                     heading:'article two',
@@ -31,18 +31,18 @@ var articles={
                     content: `
                              <p>
                                 This is the content of my second article. 
-                            </p>'
-                             };   
+                            </p>`
+                             },  
                              
                 'article-three':{
                     title:'article three',
                     heading:'article three',
                     date:'15 sep, 2016',    
-                    content: '
+                    content: `
                              <p>
                                 This is the content of my third article. 
-                            </p>'
-                             }; 
+                            </p>`
+                             },
             };
     
 function createTemplate(data){
@@ -50,32 +50,8 @@ function createTemplate(data){
     var date= data.date;
     var heading= data.heading;
     var content= data.content;
-}
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-app.get('/:articleName',function(req, res){
-    var articleName=req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
-});
-
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
-var htmlTemplate='
+    
+   var htmlTemplate=`
     <html>
     <head>
     <title> 
@@ -102,6 +78,33 @@ var htmlTemplate='
     </div>
     </body>
     </html>
-';
+`; 
+    
+}
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/:articleName',function(req, res){
+    var articleName=req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
+});
+
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
+
 return htmlTemplate;
 
